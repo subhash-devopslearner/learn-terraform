b3ed16c572458a042bbe2bb3e14487b681888744
Subhash Bhalerao <subhash.devops.learner@gmail.com>
2026-06-19 17:48:59 +0530

    Refactor Terraform configuration to set up Docker provider and remove welcome file

--- terraform-first-step/main.tf
+++ terraform-first-step/main.tf
@@ -1,5 +1,26 @@
-# This tells Terraform to create a local text file on your computer
-resource "local_file" "welcome" {
-  filename = "${path.module}/welcome.txt"
-  content  = "Welcome to the world of Infrastructure as Code!"
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+    }
+  }
+}
+
+provider "docker" {}
+
+# Pulls the Nginx image
+resource "docker_image" "nginx_img" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+# Starts the container and maps port 8080 on your machine to port 80 inside the container
+resource "docker_container" "nginx_container" {
+  image = docker_image.nginx_img.image_id
+  name  = "terraform-nginx-webserver"
+  
+  ports {
+    internal = 80
+    external = 9090
+  }
 }
